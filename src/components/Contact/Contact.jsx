abd8e6b4e20d72fef1b144dc9ed0bf985e318c02
Weston Isheim <wisheim@gmail.com>
2022-03-31 23:11:04 -0600

    Added Titles to projects/Contact styling tweeks

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,16 +10,17 @@ const handleURL = (url) => {
 const Contact = () => {
   return (
     <div className={classes.Contact} id="Contact">
-        <div className={classes.ArrowCont}>
-          <a href="#Home">
-            <img className={classes.Image} src={Arrow} alt="back to top" />
-          </a>
-        </div>
+      <div className={classes.ArrowCont}>
+        <a href="#Home">
+          <img className={classes.Image} src={Arrow} alt="back to top" />
+        </a>
+      </div>
       <div className={classes.Content}>
         <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453')} />
         <FaLinkedin size='30px' className={classes.Link} onClick={handleURL('https://www.linkedin.com/in/weston-isheim/')} />
         <FaRegFileAlt size='30px' className={classes.Link} onClick={handleURL('https://docs.google.com/document/d/1tP6pE8hQTJYv6qDqshJNE7m9ahnwPSXNpXBKaSHuc24/edit?usp=sharing')} />
       </div>
+      <p>WESTON ISHEIM &copy;</p>
     </div>
   )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
 import classes from './Projects.module.css'
+import Arrow from '../Img/UpArrow.svg'
 
 const Projects = () => {
   return (
     <div className={classes.Projects} id="Projects">
       <h2>Projects</h2>
       <div className={classes.ImageContainer}>
-        <div className={classes.Images}>
-          <div className={classes.test}></div>
-          <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
-        </div>
-        <div className={classes.Images}>
-          <div className={classes.test}></div>
+        <div className={classes.AllProjects}>
+          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <div className={classes.Images}>
+            <div className={classes.test}></div>
             <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+          </div>
         </div>
-        <div className={classes.Images}>
-          <div className={classes.test}></div>
-              <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+        <div className={classes.AllProjects}>
+          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <div className={classes.Images}>
+            <div className={classes.test}></div>
+            <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+          </div>
         </div>
-        <div className={classes.Images}>
-          <div className={classes.test}></div>
-              <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+        <div className={classes.AllProjects}>
+          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <div className={classes.Images}>
+            <div className={classes.test}></div>
+            <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+          </div>
         </div>
+        <div className={classes.AllProjects}>
+          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <div className={classes.Images}>
+            <div className={classes.test}></div>
+            <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
+          </div>
+        </div>
+        
+      </div>
+      <div className={classes.ArrowCont}>
+        <a href="#Home">
+          <img className={classes.Image} src={Arrow} alt="back to top" />
+        </a>
       </div>
     </div>
   )

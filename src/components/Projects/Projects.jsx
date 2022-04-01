7758e189029b475024def618d8dacd5436b997b2
Weston Isheim <wisheim@gmail.com>
2022-03-31 23:30:37 -0600

    Btn tweeks

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,34 +8,26 @@ const Projects = () => {
       <h2>Projects</h2>
       <div className={classes.ImageContainer}>
         <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <h2 className={classes.ProjectTitle}>Spend Tracker</h2>
           <div className={classes.Images}>
             <div className={classes.test}></div>
             <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
           </div>
         </div>
         <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <h2 className={classes.ProjectTitle}>SkiPak</h2>
           <div className={classes.Images}>
             <div className={classes.test}></div>
             <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
           </div>
         </div>
         <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Testing</h2>
+          <h2 className={classes.ProjectTitle}>Wedding Website</h2>
           <div className={classes.Images}>
             <div className={classes.test}></div>
             <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
           </div>
-        </div>
-        <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Testing</h2>
-          <div className={classes.Images}>
-            <div className={classes.test}></div>
-            <p>Donec ac mauris pellentesque, pretium nunc eget, rutrum magna. Aliquam purus est, varius et eleifend eget, volutpat in risus. Aenean lobortis volutpat urna sed fermentum. Praesent venenatis cursus viverra. Integer vel facilisis dolor. Etiam elementum dolor vel dolor consectetur finibus.</p>
-          </div>
-        </div>
-        
+        </div>    
       </div>
       <div className={classes.ArrowCont}>
         <a href="#Home">

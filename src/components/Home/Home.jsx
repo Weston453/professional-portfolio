f55d41c299d43f6dc302d1041cc82c1b411f1104
Weston Isheim <wisheim@gmail.com>
2022-04-04 18:39:57 -0600

    changed styling and added google font

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import classes from './Home.module.css'
 import { FaChevronDown } from "react-icons/fa";
-import Laptop from '../Img/Laptop.svg'
+// import Laptop from '../Img/Laptop.svg'
 import Construction from '../Img/Construction.jpg'
 
 const Home = () => {
   return (
     <div className={classes.Home} id="Home">
       <div className={classes.Container}>
-        <h1>
+        <h1 className={classes.Name}>
           Weston Isheim.
         </h1>
-        <h1>
+        <h1 className={classes.Title}>
           Full-Stack Web Developer.
         </h1>
         <div className={classes.Btn}>
@@ -23,8 +23,8 @@ const Home = () => {
           </a>
         </div>
       </div>
+      {/* <img className={classes.Laptop} src={Laptop} alt="Phone with code symbol" /> */}
       <img className={classes.Construction} src={Construction} alt="Website Construction" />
-      <img className={classes.Laptop} src={Laptop} alt="Phone with code symbol" />
     </div>
   )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,44 +14,58 @@ const Projects = () => {
     <div className={classes.Projects} id="Projects">
       <h2>Projects</h2>
       <div className={classes.ImageContainer}>
+
         <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Spend Tracker</h2>
-          <div className={classes.Images}>
-            <img className={classes.Thumbnail} src={SpendTrackerPicture} alt="Spend Tracker Project" />
-            <div className={classes.ProjectDescription}>
-              <p>This is my Devmountain Capstone project! It is a spend tracker built with a mobile first design that features user authentication, data visualization, and an intuitive UI. (Not hosted)<br/><br/>
-              React | Tailwind | Firebase | ChartJS </p>
-              <div className={classes.Icons}>
-                <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/Spend-Tracker-Capstone')} />
-                <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/eWveuxapa7w')} />
+          <div className={classes.ProjectCard}>
+            <div className={classes.ProjectHeader}>
+              <h2 className={classes.ProjectTitle}>Spend Tracker</h2>
+            </div> 
+            <div className={classes.ProjectBody}>
+              <img className={classes.Thumbnail} src={SpendTrackerPicture} alt="Spend Tracker Project" />
+              <div className={classes.ProjectDescription}>
+                <p>As my Capstone project at Devmountain, the Spend Tracker is built with a mobile-first design featuring user-authentication, data visualization, and an intuitive UI.</p>
+                <p className={classes.Skills}>React | Tailwind | Firebase | ChartJS </p>
+                <div className={classes.Icons}>
+                  <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/Spend-Tracker-Capstone')} />
+                  <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/eWveuxapa7w')} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={classes.AllProjects}>
+          <div className={classes.ProjectCard}>
+            <div className={classes.ProjectHeader}>
+              <h2 className={classes.ProjectTitle}>SkiPak</h2>
+            </div>
+            <div className={classes.ProjectBody}>
+              <img className={classes.Thumbnail} src={SkiPakPicture} alt="SkiPak Project" onClick={handleURL('http://on-the-snow.herokuapp.com/')} />
+              <div className={classes.ProjectDescription}>
+                <p>A skiing community tool, created using vanilla JS, that preforms full CRUD manipulation of DOM elements and utilizes Weatherbit API for locational weather.</p>
+                <p className={classes.Skills}>JavaScript | CSS3 | HTML5 | NodeJS | Express | Axios | Postman </p>
+                <div className={classes.Icons}>
+                  <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/skipak')} />
+                  <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/OIjiTnF7myU')} />
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>SkiPak</h2>
-          <div className={classes.Images}>
-            <img className={classes.Thumbnail} src={SkiPakPicture} alt="SkiPak Project" onClick={handleURL('http://on-the-snow.herokuapp.com/')} />
-            <div className={classes.ProjectDescription}>
-              <p>I developed this project with vanilla JS it preforms full CRUD manipulation of DOM elements and utilizes the Weatherbit  API for current locational weather. <br/><br/>
-              JavaScript | CSS3 | HTML5 | NodeJS | Express | Axios | Postman </p>
-              <div className={classes.Icons}>
-                <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/skipak')} />
-                <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/OIjiTnF7myU')} />
-              </div>
+          <div className={classes.ProjectCard}>
+            <div className={classes.ProjectHeader}>
+              <h2 className={classes.ProjectTitle}>Wedding Website</h2>
             </div>
-          </div>
-        </div>
-        <div className={classes.AllProjects}>
-          <h2 className={classes.ProjectTitle}>Wedding Website</h2>
-          <div className={classes.Images}>
-            <img className={classes.Thumbnail} src={WeddingPicture} alt="Wedding website preview" onClick={handleURL('https://koriewestonwedding.com/')}/>
-            <div className={classes.ProjectDescription}>
-              <p>I am building a day of wedding website for the guest at my wedding to find helpful info, such as the itinterary, dinner/cocktail menues, wedding party bios and a feature to find your dinner seat.<br/><br/>
-              React | Javascript | CSS3 </p>
-              <div className={classes.Icons}>
-                <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/wedding-website')} />
-                {/* <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/OIjiTnF7myU')} /> */}
+            <div className={classes.ProjectBody}>
+              <img className={classes.Thumbnail} src={WeddingPicture} alt="Wedding website preview" onClick={handleURL('https://koriewestonwedding.com/')}/>
+              <div className={classes.ProjectDescription}>
+                <p>Designed as the go-to, day-of informational source for my wedding guests, the website features a complete itinerary, dinner/cocktail menus, wedding party bios, and a database retrieval to find your dinner seat.</p>
+                <p className={classes.Skills}>React | Javascript | CSS3 </p>
+                <div className={classes.Icons}>
+                  <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/wedding-website')} />
+                  <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/OIjiTnF7myU')} />
+                </div>
               </div>
             </div>
           </div>
@@ -60,7 +74,6 @@ const Projects = () => {
       <div className={classes.ArrowCont}>
         <a href="#Home">
           <FaChevronUp className={classes.UpArrow} />
-          {/* <img className={classes.UpArrow} src={Arrow} alt="back to top" /> */}
         </a>
       </div>
     </div>

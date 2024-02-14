51954a221d6bc272bf3d0c70e7a2b8a83f14cf08
Weston Isheim <wisheim@gmail.com>
2024-02-13 19:11:40 -0500

    Removed broken skipak project URL

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -44,7 +44,8 @@ const Projects = () => {
                 <p>A skiing community tool, created using vanilla JS, that preforms full CRUD manipulation of DOM elements and utilizes Weatherbit API for locational weather.</p>
                 <p className={classes.Skills}>JavaScript | CSS3 | HTML5 | NodeJS | Express | Axios | Postman </p>
                 <div className={classes.Icons}>
-                  <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/skipak')} />
+                  {/* <FaGithub size='30px' className={classes.Link} onClick={handleURL('https://github.com/Weston453/skipak')} /> */}
+                  <FaGithub size='30px' className={classes.Link} />
                   <FaYoutube size='30px' className={classes.Link} onClick={handleURL('https://youtu.be/OIjiTnF7myU')} />
                 </div>
               </div>
